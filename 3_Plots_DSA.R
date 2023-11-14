720bb7545b6ea1f7a05814158ae836d396c793fe
Thao P Le <tk.le@unimelb.edu.au>
2023-11-14 13:59:07 +1100

    made a tornado diaram for high TP in older population

--- 3_Plots_DSA.R
+++ 3_Plots_DSA.R
@@ -108,6 +108,65 @@ ggsave(height=6, width=9, dpi=600, file="plots/figure_E5.pdf")
 
 
 
+##### Fig E5 - high TP version  ##################
+
+# Get the base case icer value
+baseCase <- covidData_Base %>%  
+  filter(group=="A" & immuneEscape=="1.50 yr" & tpLevel=="high TP" & 
+           scenario=="High-risk boost" & boostStart=="2.00 yr") %>% 
+  .$icer
+baseCase
+
+# Filter scenarios needed and use better parameter names
+df <- covidData_OWSA %>%  
+  filter(group=="A" & immuneEscape=="1.50 yr" & tpLevel=="high TP" & 
+           scenario=="High-risk boost" & boostStart=="2.00 yr") %>% 
+  select (-icerDonated) %>%
+  pivot_wider(names_from = result, values_from = icer) %>% 
+  mutate(range = abs(High - Low)) %>% 
+  arrange(range) %>% 
+  mutate(parameter = as.character(parameter)) %>% # Convert parameter to character data type
+  mutate(parameter = replace(parameter, parameter=="Cost vax delivery (A)", "Cost of vaccine delivery")) %>%
+  mutate(parameter = replace(parameter, parameter=="Cost home-based (A)", "Cost of home-based care")) %>%
+  mutate(parameter = replace(parameter, parameter=="Cost bedday ICU (A)", "Cost per ICU bed day")) %>%
+  mutate(parameter = replace(parameter, parameter=="Cost bedday ward (A)", "Cost per non-ICU bed day")) %>%
+  mutate(parameter = replace(parameter, parameter=="Cost vaccine dose (A)", "Cost of vaccine per dose")) %>%
+  mutate(parameter = replace(parameter, parameter=="Days sick moderate", "Duration of illness, moderate")) %>%
+  mutate(parameter = replace(parameter, parameter=="Days sick postacute", "Duration of illness, post-acute")) %>%
+  mutate(parameter = replace(parameter, parameter=="Prop. doses wasted", "Dose wastage proportion")) %>%
+  mutate(parameter = replace(parameter, parameter=="Dis. weight postacute", "Disability Weight, post-acute")) %>%
+  mutate(parameter = replace(parameter, parameter=="Dis. weight moderate", "Disability Weight, moderate")) %>%
+  mutate(parameter=factor(x=parameter, levels=parameter)) %>% 
+  pivot_longer(names_to='type', values_to='value', Low:High) %>% 
+  slice_tail(n=20) # keep only the last 20 observations
+
+df <- df %>%
+  mutate(type = replace(type, type=="High", "High value input")) %>% 
+  mutate(type = replace(type, type=="Low", "Low value input"))
+
+# Make tornado plot
+ggplot(df, aes(x=parameter,y=value, fill=type, colour=type)) +
+  geom_bar(data=df[df$type=="Low value input",],  aes(x=parameter,y=value), stat="identity", linewidth=0.1) +
+  geom_bar(data=df[df$type=="High value input",], aes(x=parameter,y=value), stat="identity", linewidth=0.1) +
+  geom_hline(yintercept = baseCase, linetype = "solid", linewidth=0.25) +
+  theme + xscale + coord_flip() + border + scaleFill + scaleColor +
+  scale_y_continuous(name="Incremental cost-effectiveness ratio ($)", trans=offset_trans(offset=baseCase),  breaks=breaks_extended(7) ) + #, 
+                     # limits = c(1000, 10000), breaks=breaks_extended(7)) +
+  geom_text(data = subset(df, type=="Low value input"), show.legend = FALSE, 
+            aes(label = comma(round(value))), 
+            hjust= ifelse(subset(df, type=="Low value input", select = value) < baseCase, 1.15, -0.15),
+            size = 4, family = "univers") +
+  geom_text(data = subset(df, type=="High value input"), show.legend = FALSE, 
+            aes(label = comma(round(value))), 
+            hjust= ifelse(subset(df, type=="High value input", select = value) < baseCase, 1.15, -0.15),
+            size = 4, family = "univers") +
+  ggtitle("Scenario: older population, 80% initial vaccination coverage, high TP \n immune escape starts 1.5 yr, boosting at 2.0 yr")
+
+ggsave(height=6, width=9, dpi=600, file="plots/figure_E5_high_TP.pdf")
+
+
+
+
 ##### Fig E6  ##################
 
 # Get the base case icer value
